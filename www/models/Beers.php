8845ea14140002442c518edb5c8b23da2b971bc6
24Syleo <sylvain.beggiora@outlook.com>
2023-05-04 12:02:54 +0200

    structure du code

---
+++ www/models/Beers.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class Beers{
+    private $connection = null;
+
+    /**
+     * Constructor - Connect to the database.
+     */
+    public function __construct()
+    {
+      try {
+        // Connect to the database.
+        $this->connection = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE_NAME, DB_USERNAME, DB_PASSWORD);
+      } catch(PDOException $e) {
+        // Send an error because it could not connect to the database.
+        throw new Exception($e->getMessage());
+      }      
+    }
+
+    public function searchBeers(){}
+
+    public function createBeer(){}
+
+    public function readBeer(){}
+
+    public function updateBeer(){}
+
+    public function deleteBeer(){}
+
+}
