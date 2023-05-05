98943d4a4a3fa620ad86d8f1ac32ade8e317d589
BaoBaB-laBs <126256031+BaoBaB-laBs@users.noreply.github.com>
2023-05-05 12:31:04 +0200

    clean

--- www/controller.php
+++ www/controller.php
@@ -5,31 +5,15 @@ require_once __DIR__ . "/models/Ingredients.php";
 // Récupération des constantes d'accès pour la base de données
 require_once "./config.php";
 
-
-
 //récupère le chemin appelé
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 //Choisir le controller a appelé en fonction du chemin
-$uri === "/beers" ? $res = manageBeers() : $res = manageIngredients();
-//$uri == "/beers" ? $res = manageBeers() : $res = manageIngredients();
-
-
-
 if (preg_match('#^/beers#', $uri)) {
     $res = manageBeers();
 } else {
     $res = manageIngredients();
 }
-
-var_dump('uri : ' . $uri);
-//$body = json_decode(file_get_contents('php://input'));
-var_dump($_SERVER['QUERY_STRING']);
-
-parse_str($_SERVER['QUERY_STRING'], $query);
-
-
-
 
 /**
  * manageBeers
@@ -40,8 +24,14 @@ function manageBeers(){
     $beer = new Beers();
     $method = $_SERVER['REQUEST_METHOD'];
     $body = json_decode(file_get_contents('php://input'), true);
+    parse_str($_SERVER['QUERY_STRING'], $query);
+    $id = isset($query['id']) ? $query['id'] : '';
     
     switch($method) {
+        case 'GET':
+            if ($id) {
+                $beer->readBeer($id);
+            }
         case 'POST':
             try {
             //controler les entrées
@@ -91,21 +81,7 @@ function manageBeers(){
             die($e);
           }
     }
-function manageBeers()
-{
-    $beer = new Beers();
-    $method = $_SERVER['REQUEST_METHOD'];
-    $id = isset($query['id']) ? $query['id'] : '';
-    var_dump('id : ' . $id);
 
-
-    switch ($method) {
-        case 'GET':
-            if ($id) {
-                $beer->readBeer($id);
-            }
-    }
-}
 /**
  * manageIngredients
  *
