2caef77b898ef978e43af10cc78b3aa45647f0eb
BaoBaB-laBs <126256031+BaoBaB-laBs@users.noreply.github.com>
2023-05-08 10:32:28 +0200

    added updateIngredients

--- www/models/Ingredients.php
+++ www/models/Ingredients.php
@@ -23,7 +23,25 @@ class Ingredients{
 
     public function readIngredient(){}
 
-    public function updateIngredient(){}
+    public function updateIngredient ($array, $id)
+  {
+    try {
+      //recuperer chaque valeur
+      $tab = [];
+      foreach ($array as $ar) {
+        array_push($tab, $ar);
+      }
+
+     
+
+      $sql = "UPDATE ingredients SET name=?,tagline=?,first_brewed=?,description=?,image_url=?,brewers_tips=?,contributed_by=?,food_pairing=?,food_pairing2=?,food_pairing3=? WHERE id=?";
+      $stmt = $this->connection->prepare($sql);
+      $stmt->execute(array($tab[0], $tab[1], $tab[2], $tab[3], $tab[4], $tab[6], $tab[7], $tabFood[0], $tabFood[1], $tabFood[2], $id));
+      return $this->readBeer($id);
+    } catch (Exception $e) {
+      throw $e;
+    }
+  }
 
     public function deleteIngredient(){}
 
