5c9b77f631ba3e9f5a101e5026b041a350cf83ea
24Syleo <sylvain.beggiora@outlook.com>
2023-05-11 09:17:33 +0200

    ok

--- www/controller.php
+++ www/controller.php
@@ -117,38 +117,43 @@ function manageBeers()
       }
       elseif($id){
         $beerData = $beer->readBeer($id);
-        $beerList = [];
-        foreach ($beerData as $beer) {
-          $beerId = $beer->beer_id;
-          if (!isset($beerList[$beerId])) {
-            $beerList[$beerId] = [
-              "id" => $beer->beer_id,
-              "name" => $beer->name,
-              "tagline" => $beer->tagline,
-              "first_brewed" => $beer->first_brewed,
-              "description" => $beer->description,
-              "image_url" => $beer->image_url,
-              "ingredients" => [],
-              "brewers_tips" => $beer->brewers_tips,
-              "contributed_by" => $beer->contributed_by,
-              "food_pairing" => $beer->food_pairing,
-              "food_pairing2" => $beer->food_pairing2,
-              "food_pairing3" => $beer->food_pairing3
+        if(count($beerData) == 1){
+          return $beerData;
+        }
+        else{
+
+          $beerList = [];
+          foreach ($beerData as $beer) {
+            $beerId = $beer->beer_id;
+            if (!isset($beerList[$beerId])) {
+              $beerList[$beerId] = [
+                "id" => $beer->beer_id,
+                "name" => $beer->name,
+                "tagline" => $beer->tagline,
+                "first_brewed" => $beer->first_brewed,
+                "description" => $beer->description,
+                "image_url" => $beer->image_url,
+                "ingredients" => [],
+                "brewers_tips" => $beer->brewers_tips,
+                "contributed_by" => $beer->contributed_by,
+                "food_pairing" => $beer->food_pairing,
+                "food_pairing2" => $beer->food_pairing2,
+                "food_pairing3" => $beer->food_pairing3
+              ];
+            }
+            $beerList[$beerId]['ingredients'][] = [
+              "id" => $beer->ingredient_id,
+              "name" => $beer->name_ing,
+              "type" => $beer->type,
+              "amount_value" => $beer->amount_value,
+              "amount_unit" => $beer->amount_unit,
+              "amount_add" => $beer->amount_add,
+              "amount_attribute" => $beer->amount_attribute,
+              
             ];
           }
-          $beerList[$beerId]['ingredients'][] = [
-            "id" => $beer->ingredient_id,
-            "name" => $beer->name_ing,
-            "type" => $beer->type,
-            "amount_value" => $beer->amount_value,
-            "amount_unit" => $beer->amount_unit,
-            "amount_add" => $beer->amount_add,
-            "amount_attribute" => $beer->amount_attribute,
-
-          ];
+          return $beerList;
         }
-
-        return $beerList;
       } else {
         if ($queryPerPage) {
           $queryLimit = $queryPerPage;
